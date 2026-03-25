95bcf9b5ef60bce0a5b3824947e76a6529fc232b
Nickolay Gusev <nickolay.gusev@innowise.com>
2026-03-25 12:33:02 +0100

    Initial commit

---
+++ src/main/java/com/idneo/idneotest/IdneotestApplication.java
@@ -0,0 +1,13 @@
+package com.idneo.idneotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdneotestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdneotestApplication.class, args);
+    }
+
+}
